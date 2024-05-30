6979f0d5548ad1c29c57aae07540ffb4bb35605f
mnandamello <mfernanda.nintendo@gmail.com>
2024-05-30 07:48:46 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PURE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
